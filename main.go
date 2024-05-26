78c773e2ada22fa42958e62311d1187926e4d975
nutcas3 <mauricee423@gmail.com>
2024-05-26 21:03:25 +0300

    added packages

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog"
+	"log"
+	"net/http"
+	"os"
+	"paystack/handler"
+)
+
 func main()  {
 	
 }
