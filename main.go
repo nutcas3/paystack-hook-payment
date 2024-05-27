60a9ce3cc7b7d3b60b288910347622ca15fd3f9c
nutcas3 <mauricee423@gmail.com>
2024-05-27 18:30:08 +0300

    Implement server startup logic and route configuration
    This commit establishes the core server startup logic and configures routes for the Paystack transaction verification application.
    
        Environment variable loading:
            It attempts to load environment variables using
    
            godotenv.Load(). In case of errors, it logs a fatal error message with details using log.Fatalf.
        API logger setup:
            It creates a new Zerolog logger (apiLogger) and sets its output to standard out (os.Stdout).
        Router initialization:
            It initializes a Gin router instance (myRouter) using gin.Default().
        Port configuration:
            It retrieves the port number from the environment variable "PORT".
            If the "PORT" variable is missing, it panics with a message indicating the missing configuration.
        Server startup logging:
            The logger outputs an informational message (apiLogger.Info().Msgf) indicating the server is running on the specified port retrieved from the environment.
        Route configuration:
            It configures two routes:
                GET route at "/transaction/check" that maps to the handler.CheckTransaction function for transaction verification.
                POST route at "/transaction/webhook" that maps to the handler.WebHook function for handling Paystack webhooks.
        Server start:
            It starts the server using http.ListenAndServe, listening on the specified port and serving requests using the configured router.
            In case of errors during server startup, it logs a fatal error message with details using log.Fatalf.
    
    This commit lays the foundation for the application to run and handle incoming requests for transaction verification and webhook processing.

--- main.go
+++ main.go
@@ -12,4 +12,23 @@ import (
 
 func main()  {
 	
+	if Er := godotenv.Load(); Er != nil {
+		log.Fatalf("error loading .env file (%s)", Er.Error())
+	}
+
+	apiLogger := zerolog.New(os.Stdout)
+	myRouter := gin.Default()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		panic("PORT missing in env")
+	}
+
+	apiLogger.Info().Msgf("server running at port %s", port)
+	myRouter.GET("/transaction/check", handler.CheckTransaction)
+	myRouter.POST("/transaction/webhook", handler.WebHook)
+
+	if er := http.ListenAndServe(":"+port, myRouter); er != nil {
+		log.Fatalf("unable to listen and serve at port %s", port)
+	}
 }
